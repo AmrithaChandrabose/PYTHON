f15b9dfa56c7d3d8fab6f0bbcbc06b0964044c66
AmrithaChandrabose <77480987+AmrithaChandrabose@users.noreply.github.com>
2021-04-14 22:04:21 -0700

    Delete lab1.1-2.py

--- lab1.1-2.py
+++
@@ -1,8 +0,0 @@
-list=[] 
-n=int(input("Enter the no.of integers:")) 
-for i in range(0,n):
-    list.append(int(input("Enter the integer:"))) 
-print("Postive integers are") 
-for i in list: 
-    if(i>0): 
-        print(i,end=" ")
