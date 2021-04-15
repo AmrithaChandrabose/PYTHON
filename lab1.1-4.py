53eba7edb60b63ee7dcf10ece7b7e95ab226e1bc
AmrithaChandrabose <77480987+AmrithaChandrabose@users.noreply.github.com>
2021-04-14 22:04:40 -0700

    Delete lab1.1-4.py

--- lab1.1-4.py
+++
@@ -1,7 +0,0 @@
-list=[] 
-vowels=['a','e','i','o','u']
-string=input("Enter a word:") 
-for i in string:
-    if((i in vowels)and(i not in list)): 
-       list.append(i)
-print("Vowels present in given word:",list)
